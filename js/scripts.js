18e00dfa0717f50144b68886d037c2d11c9660bf
gonalvarado <gonalvarado86@gmail.com>
2024-06-28 09:35:24 -0300

    subiendo carga dinámica de tabla administrador

--- js/scripts.js
+++ js/scripts.js
@@ -56,3 +56,67 @@ var combo = document.getElementById("ciudad");
 var seleccion = combo.options[combo.selectedIndex].text;
 console.log(seleccion);
 }
+
+// funcion que crea las filas de la tabla administrador
+function crearFila (destino) {
+    // crea la fila 
+    const tr = document.createElement('tr');
+    // crea las celdas
+    const tdNombre = document.createElement('td');
+    const tdProvincia = document.createElement('td');
+    const tdImagen = document.createElement('td');
+    const tdBotones = document.createElement('td');
+    // agrega datos a las celdas
+    tdNombre.innerHTML = destino.nombre;
+    tdProvincia.innerHTML = destino.provincia;
+    // crea la imagen de la celda
+    const tdImg = document.createElement('img');
+    tdImg.src = `../assets/img/${destino.imagen}`;
+    // crea los botones de la celda
+    const tdBtnMod = document.createElement('button');
+    tdBtnMod.classList.add('mod');
+    tdBtnMod.innerHTML = 'Modificar';
+    const tdBtnEli = document.createElement('button');
+    tdBtnEli.innerHTML = 'Eliminar';
+    tdBtnEli.classList.add('elim');
+    // agrega los elementos a la fila 
+    tdImagen.appendChild(tdImg);
+    tdBotones.appendChild(tdBtnMod);
+    tdBotones.appendChild(tdBtnEli);
+    tr.appendChild(tdNombre);
+    tr.appendChild(tdProvincia);
+    tr.appendChild(tdImagen);
+    tr.appendChild(tdBotones);
+
+    return tr;
+}
+
+// datos de la api
+const API_SERVER = 'http://127.0.0.1:8080/destinosapi/destinos';
+const options = {
+    method: 'GET'
+};
+
+// Función para cargar destinos en la tabla del administrador
+const cargarDestinos = async () => {
+    try{
+        // Realizamos una petición fetch a la API para obtener los destinos
+        const response = await fetch(`${API_SERVER}`, options);
+        // Convertimos la respuesta a JSON
+        const data = await response.json();
+        // Capturamos la tabla   
+        const bodyTabla = document.getElementById('bodyTabla');
+        // Limpiamos el contenido previo de la tabla
+        bodyTabla.innerHTML = '';
+        // Iteramos sobre los destinos y los agregamos a la tabla
+        data.forEach(place => {
+            const destinoTr = crearFila(place);
+            bodyTabla.appendChild(destinoTr);
+        });
+    }catch(error){
+        console.error(error);
+    }
+};
+      
+// Llamar a la función para cargar los destinos cuando el DOM esté cargado
+document.addEventListener("DOMContentLoaded", () => { cargarDestinos()});
